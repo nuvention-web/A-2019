19902c7c3d5776a5a6dbcaff605a76383ebf9724
HW <374154361@qq.com>
2019-02-26 14:32:54 -0600

    modified homepage slogan

--- pro-fashion/src/components/Home/index.js
+++ pro-fashion/src/components/Home/index.js
@@ -6,7 +6,7 @@ import Album from '../Album/album.js';
 const HomePage = () => (
   <div>
     <h1>Home Page</h1>
-    <p>The Home Page is accessible by every signed in user.</p>
+    <p>Welcome to ProFashion. See the outfit we pick for you!</p>
     <Album />
   </div>
 );
